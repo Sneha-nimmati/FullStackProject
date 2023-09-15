0177eab4845f4902eff43345240ead90424cd137
Sneha-nimmati <98114358+Sneha-nimmati@users.noreply.github.com>
2023-09-15 17:05:30 -0400

    Update admin.py

--- universities/homepage/admin.py
+++ universities/homepage/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Universitieslist)
+admin.site.register(CourseNames)
+admin.site.register(Subjects)
 
